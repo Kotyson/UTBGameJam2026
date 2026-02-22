867cd8112e5025f647e15844e4d1f25b68e6256b
JelosGit <jjellda@gmail.com>
2026-02-22 13:55:38 +0100

    logika hotova

--- Assets/David/Scripts/LevelData.cs
+++ Assets/David/Scripts/LevelData.cs
@@ -1,6 +1,6 @@
 public static class LevelData
 {
-    
     public static int Player1Score { get; set; }
     public static int Player2Score { get; set; }
+    public static string LastSceneName { get; set; } // Tady si zapamatujeme název levelu
 }
